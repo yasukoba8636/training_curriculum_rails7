7282f0391830cc26335a285cc15514e43579eaaf
yasukoba8636 <yasukobatenri@gmail.com>
2025-09-27 22:46:51 +0900

    issue1~6修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -9,7 +9,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-  
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -24,8 +23,7 @@ class CalendarsController < ApplicationController
   def getWeek
   
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    wdays[3]
-  
+    
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -35,17 +33,13 @@ class CalendarsController < ApplicationController
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
-      today_plans = []
-      plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
-      end
+      today = @todays_date + x
 
-       wday_num = Date.today.wday
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
+      # 今日の予定だけを配列にまとめる
+      today_plans = plans.select { |plan| plan.date == today }.map(&:plan)
+
     
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday =>wdays[(@todays_date + x).wday]}
+      days = { month: today.month, date: today.day, plans: today_plans, wday: wdays[today.wday]}
 
       @week_days.push(days)
     end
